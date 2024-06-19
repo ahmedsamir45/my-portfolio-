1998ea09e3c8beb42b8574e5a52d75be97501f33
ahmedsamir45 <ahmedsamer6788@gmail.com>
2024-06-19 03:42:18 +0300

    update1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-window.alert("it just a simple version the website will be created soon")
+
+nav = document.getElementsByTagName("nav")
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = "flex"
+    // sidebar.classList.add("activesidebar")
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = "none"
+    // sidebar.classList.remove("activesidebar")
+  }
+links = document.querySelectorAll(".sidebar  li a")
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    hideSidebar()
+  })
+})
